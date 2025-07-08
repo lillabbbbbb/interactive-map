5fb0650dae20ef874b0c3ceed795d68e86a8febe
lillabbbbbb <b.lilla6279@gmail.com>
2025-07-08 17:43:15 +0300

    fetch implementation, styling and onclick binding is in progress

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const fetchStatFinData = async (url, body) => {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    return await response.json()
+}
+
 const fetchGeo = async () => {
     const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
     const response = await fetch(url)
@@ -13,7 +24,9 @@ const initMap = (data) => {
     })
 
     let geoSJON = L.geoJSON(data, {
-        weight: 2
+        weight: 2,
+        onEachFeature: getFeature,
+        //style: getStyle
     }).addTo(map)
 
     let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -34,12 +47,37 @@ const initMap = (data) => {
 }
 
 const fetchMigrations = async () => {
-    let url = "https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/muutl/statfin_muutl_pxt_11a2.px";
-    const response = await fetch(url)
-    const migrationJSON = await response.json();
+    
+    let populationURL = "https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/muutl/statfin_muutl_pxt_11a2.px";
+    const populationBody = await (await fetch("population_query.json")).json()
+
+    const populationData = fetchStatFinData(populationURL, populationBody)
+
+    console.log(populationData)
 
     
 }
 
+const getFeature = (feature, layer) => {
+    if(!feature.properties.id) return;
+    const id = feature.properties.id
+    //console.log(feature.properties)
+    //console.log(id)
+
+    layer.bindPopup(
+        `<p>${feature.properties.name}</p>
+        <p>Positive: ${id}</p>
+        <p>Negative: ${id}</p>`
+    )
+}
+
+const getStyle = (feature) => {
+    if(!feature.properties.id) return;
+    return {
+        color: `hsl(${Math.pow((posMig / negMig),3) * 60}, 75%, 50%)`
+    }
+}
+
 
 fetchGeo();
+fetchMigrations();
